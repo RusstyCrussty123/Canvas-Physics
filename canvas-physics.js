0eb1c00fc90eb96ba02b0274bf21f1679a5b48cd
RustyCrusty <61127918+RusstyCrussty123@users.noreply.github.com>
2020-06-17 14:31:53 -0500

    Update canvas-physics.js

--- canvas-physics.js
+++ canvas-physics.js
@@ -13,15 +13,15 @@ function fall(index, index2){
     var top = collide.y - collide.h
     if (part.y != top){
         remove(part)
-        update(part.x, part.y + 1, part.h, part.w, part.static, part.color, index)
+        update(part.x, part.y + 1, part.w, part.h, part.static, part.color, index)
     }else if(part.x - part.w/2 > collide.x + collide.w/2 || part.x + part.w/2 < collide.x - collide.w/2 ) {
         remove(part)
-        update(part.x, part.y + 1, part.h, part.w, part.static, part.color, index)
+        update(part.x, part.y + 1, part.w, part.h, part.static, part.color, index)
     }
 }
 function remove(part){
     ctx.fillStyle = canvasColor
-    ctx.fillRect(part.x, part.y, part.h, part.w)
+    ctx.fillRect(part.x, part.y, part.w, part.h)
 }
 function create(x,y,w,h,col,stat){ // a=x, b=y, c=w, d=h
     ctx.fillStyle = col
@@ -29,17 +29,17 @@ function create(x,y,w,h,col,stat){ // a=x, b=y, c=w, d=h
     objects.push({x:x, y:y, w:w, h:h, static:stat, color:col})
 }
 
-function update(a,b,c,d,col,stat, index){
+function update(x,y,w,h,col,stat, index){
     ctx.fillStyle = col
-    ctx.fillRect(a, b, c, d)
-    objects[index] = {x:a, y:b, h:c, w:d, static:stat, color:col}
+    ctx.fillRect(x, y, w, h)
+    objects[index] = {x:x, y:y, w:w, h:h, static:stat, color:col}
 }
 
-function makeCanvas(h, w, color, id){
+function makeCanvas(w, h, color, id){
     canvas = document.getElementById(id);
     ctx = canvas.getContext("2d");
 
     canvasColor = color
     ctx.fillStyle = color;
-    ctx.fillRect(0, 0, h, w);
+    ctx.fillRect(0, 0, w, h);
 }
