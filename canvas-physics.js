01b0d7dbe50dabfea93150ef59d1f79cbcacaafb
RustyCrusty <61127918+RusstyCrussty123@users.noreply.github.com>
2020-06-24 15:45:46 -0500

    Update canvas-physics.js

--- canvas-physics.js
+++ canvas-physics.js
@@ -11,10 +11,13 @@ function fall(index, index2){
     var part = objects[index]
     var collide = objects[index2]
     var top = collide.y - collide.h
+    var quotient1
+    var quotient2
+
     if (part.y != top){
         remove(part)
         update(part.x, part.y + 1, part.w, part.h, part.static, part.color, index)
-    }else if(part.x - part.w/2 > collide.x + collide.w/2 || part.x + part.w/2 < collide.x - collide.w/2 ) {
+    }else if(part.x > collide.x + collide.w || part.x + part.h < collide.x) {
         remove(part)
         update(part.x, part.y + 1, part.w, part.h, part.static, part.color, index)
     }
